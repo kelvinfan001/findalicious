8b09c3ec929d050bb0b11b88daf4f1bf4a7f3478
Kelvin Fan <kelvinfan001@gmail.com>
2020-04-23 21:31:07 -0400

    fix res.json() bug

--- react/build/precache-manifest.d2faaee68b6a9a32f0e7c2ee23b7d74f.js
+++ react/build/precache-manifest.d2faaee68b6a9a32f0e7c2ee23b7d74f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e1a2c4cf949c5cfdf8f56b540bee278c",
+    "revision": "8684996673420905d44679d62bca901d",
     "url": "/index.html"
   },
   {
-    "revision": "77dcb0407cbdf11f8683",
+    "revision": "c4d99a5951c82e638ebe",
     "url": "/static/css/main.749e99e8.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.067299f9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "77dcb0407cbdf11f8683",
-    "url": "/static/js/main.75a14a3b.chunk.js"
+    "revision": "c4d99a5951c82e638ebe",
+    "url": "/static/js/main.b7906a13.chunk.js"
   },
   {
     "revision": "702554416a81a1e3a894",
