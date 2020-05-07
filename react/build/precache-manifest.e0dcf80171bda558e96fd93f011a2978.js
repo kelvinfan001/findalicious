848da921141d5508deb066f2c0e4e60dc64521e6
Kelvin Fan <kelvinfan001@gmail.com>
2020-05-07 00:08:10 -0400

    switch to heroku

--- react/build/precache-manifest.e0dcf80171bda558e96fd93f011a2978.js
+++ react/build/precache-manifest.e0dcf80171bda558e96fd93f011a2978.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "29ccf7796e7e41ff9ff481856b1825d8",
+    "revision": "faf22cecd5bd556e86dda5e2ee35e6f0",
     "url": "/index.html"
   },
   {
-    "revision": "d3e0213bcdff3724bf8d",
+    "revision": "628df40bb332ebcce89f",
     "url": "/static/css/main.68e1e55c.chunk.css"
   },
   {
-    "revision": "e6c24bd0ea0064ffec9b",
-    "url": "/static/js/2.a096908d.chunk.js"
+    "revision": "ae901842d6e754e1d2db",
+    "url": "/static/js/2.08e62341.chunk.js"
   },
   {
     "revision": "a210d5c8f38e87d25868ec3f57e5bb12",
-    "url": "/static/js/2.a096908d.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.08e62341.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d3e0213bcdff3724bf8d",
-    "url": "/static/js/main.ee509ad1.chunk.js"
+    "revision": "628df40bb332ebcce89f",
+    "url": "/static/js/main.012901cb.chunk.js"
   },
   {
     "revision": "702554416a81a1e3a894",
