3e6b1d1a06ff06ac212384f597c0ea372deb4bfe
Kelvin Fan <kelvinfan001@gmail.com>
2020-06-11 16:52:15 -0400

    build frontend

--- react/build/precache-manifest.306700a2479044bc9d1ecf74cdfebf3d.js
+++ react/build/precache-manifest.306700a2479044bc9d1ecf74cdfebf3d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c894b10b44bcbe2abb0032852f60fdb",
+    "revision": "e1bf8641586f4a8fd8337f19d17767cb",
     "url": "/index.html"
   },
   {
-    "revision": "0dee158535ad48dfdf29",
+    "revision": "20bd81cf5b03abb95ca3",
     "url": "/static/css/main.54558af5.chunk.css"
   },
   {
-    "revision": "a6c978b54e00cbcfc322",
-    "url": "/static/js/2.c08c12f6.chunk.js"
+    "revision": "faf3c58f60cf80bf4351",
+    "url": "/static/js/2.d70d5f7a.chunk.js"
   },
   {
     "revision": "14849b535301c3a8f1b480bdbd969d96",
-    "url": "/static/js/2.c08c12f6.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.d70d5f7a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0dee158535ad48dfdf29",
-    "url": "/static/js/main.dfdcfb7b.chunk.js"
+    "revision": "20bd81cf5b03abb95ca3",
+    "url": "/static/js/main.96e5645e.chunk.js"
   },
   {
     "revision": "702554416a81a1e3a894",
