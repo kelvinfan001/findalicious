91943413cd7c714a06280d02b82104e33845a1a8
Kelvin Fan <kelvinfan001@gmail.com>
2020-12-27 19:46:50 -0500

    style: Various linting fixes
    Fix some non airbnb-related errors to get us to at least compile.
    This is only a very small subset of linting errors that eventually
    need to be fixed.

--- react/src/components/NotFound/NotFound.js
+++ react/src/components/NotFound/NotFound.js
@@ -1,54 +1,54 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 function PageNotFound() {
-    return (
-        <div className="main-page">
-            <h2>Page Not Found</h2>
-            <Link to="/">
-                <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" />
-            </Link>
-        </div>
-    );
+  return (
+    <div className="main-page">
+      <h2>Page Not Found</h2>
+      <Link to="/">
+        <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" />
+      </Link>
+    </div>
+  )
 }
 
 function RoomNotFound() {
-    return (
-        <div className="main-page">
-            <h2>Room Not Found</h2>
-            <p>Please make sure you've entered a valid room ID in the URL   </p>
-            <Link to="/">
-                <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" />
-            </Link>
-        </div>
-    );
+  return (
+    <div className="main-page">
+      <h2>Room Not Found</h2>
+      <p>Please make sure you&apos;ve entered a valid room ID in the URL </p>
+      <Link to="/">
+        <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" />
+      </Link>
+    </div>
+  )
 }
 
 function UserDisconnect() {
-    return (
-        <div className="main-page">
-            <h2>User Disconnected</h2>
-            <p>A user in your room has disconnected.</p>
-            <p>Tip: Make sure your friends don't refresh their browsers while swiping. </p>
-            <Link to="/">
-                <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" />
-            </Link>
-        </div>
-    );
+  return (
+    <div className="main-page">
+      <h2>User Disconnected</h2>
+      <p>A user in your room has disconnected.</p>
+      <p>Tip: Make sure your friends don&apos;t refresh their browsers while swiping. </p>
+      <Link to="/">
+        <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" />
+      </Link>
+    </div>
+  )
 }
 
 function RoomAlreadyStartedSwiping() {
-    return (
-        <div className="main-page">
-            <h2>Room Already Swiping</h2>
-            <p>Make sure you are in the room before your friends press "EVERYONE IS IN".</p>
-            <Link to="/">
-                <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" />
-            </Link>
-        </div>
-    );
+  return (
+    <div className="main-page">
+      <h2>Room Already Swiping</h2>
+      <p>Make sure you are in the room before your friends press &quot;EVERYONE IS IN&quot;.</p>
+      <Link to="/">
+        <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" />
+      </Link>
+    </div>
+  )
 }
 
-export { PageNotFound, RoomNotFound, UserDisconnect, RoomAlreadyStartedSwiping };
+export { PageNotFound, RoomNotFound, UserDisconnect, RoomAlreadyStartedSwiping }
--- react/src/react-swipeable-cards/es/Card.js
+++ react/src/react-swipeable-cards/es/Card.js
@@ -1,98 +1,121 @@
-import React, { Component } from "react";
-import Hammer from "react-hammerjs";
+import React, { Component } from 'react'
+import Hammer from 'react-hammerjs'
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError('Cannot call a class as a function')
+  }
+}
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _possibleConstructorReturn(self, call) {
+  if (!self) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called")
+  }
+  return call && (typeof call === 'object' || typeof call === 'function') ? call : self
+}
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== 'function' && superClass !== null) {
+    throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass)
+  }
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: { value: subClass, enumerable: false, writable: true, configurable: true }
+  })
+  if (superClass)
+    Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : (subClass.__proto__ = superClass)
+}
 
+let Card = (function (_Component) {
+  _inherits(Card, _Component)
 
-var Card = function (_Component) {
-	_inherits(Card, _Component);
+  function Card(props) {
+    _classCallCheck(this, Card)
 
-	function Card(props) {
-		_classCallCheck(this, Card);
+    let _this = _possibleConstructorReturn(this, _Component.call(this, props))
 
-		var _this = _possibleConstructorReturn(this, _Component.call(this, props));
+    _this.state = {
+      classList: ['card_container']
+    }
+    return _this
+  }
 
-		_this.state = {
-			classList: ["card_container"]
-		};
-		return _this;
-	}
+  Card.prototype.componentDidMount = function componentDidMount() {}
 
-	Card.prototype.componentDidMount = function componentDidMount() { };
+  Card.prototype.onPan = function onPan(event) {
+    if (this.props.isSwipeEnabled !== false) {
+      if (
+        (this.props.isLeftSwipeEnabled !== false && event.deltaX <= 0) ||
+        (this.props.isRightSwipeEnabled !== false && event.deltaX >= 0)
+      ) {
+        this.state.classList.push('moving')
+        if (event.deltaX === 0) return
+        if (event.center.x === 0 && event.center.y === 0) return
+        let xMulti = event.deltaX * 0.08 // 0.03
+        let yMulti = 1 // event.deltaY / 80
+        let rotate = xMulti * yMulti
+        // console.log(event.deltaY)
+        // event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
+        event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + 10 + 'px) rotate(' + rotate + 'deg)'
+      }
+    }
+  }
 
-	Card.prototype.onPan = function onPan(event) {
-		if (this.props.isSwipeEnabled !== false) {
-			if (this.props.isLeftSwipeEnabled !== false && event.deltaX <= 0 || this.props.isRightSwipeEnabled !== false && event.deltaX >= 0) {
-				this.state.classList.push('moving');
-				if (event.deltaX === 0) return;
-				if (event.center.x === 0 && event.center.y === 0) return;
-				var xMulti = event.deltaX * 0.08; // 0.03
-				var yMulti = 1; // event.deltaY / 80
-				var rotate = xMulti * yMulti;
-				// console.log(event.deltaY)
-				// event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
-				event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + 10 + 'px) rotate(' + rotate + 'deg)';
-			}
-		}
-	};
+  Card.prototype.onPanEnd = function onPanEnd(event) {
+    if (this.props.isSwipeEnabled !== false) {
+      if (
+        (this.props.isLeftSwipeEnabled !== false && event.deltaX <= 0) ||
+        (this.props.isRightSwipeEnabled !== false && event.deltaX >= 0)
+      ) {
+        let newClass = this.state.classList.filter(function (s) {
+          return s !== 'moving'
+        })
+        this.setState({ classList: newClass })
+        let moveOutWidth = document.body.clientWidth
+        let keep = Math.abs(event.deltaX) < 80
+        event.target.classList.toggle('removed', !keep)
+        if (keep) {
+          event.target.style.transform = ''
+        } else {
+          let endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth)
+          let toX = event.deltaX > 0 ? endX : -endX
+          let endY = Math.abs(event.velocityY) * moveOutWidth
+          let toY = event.deltaY > 0 ? endY : -endY
+          let xMulti = event.deltaX * 0.08 // 0.03
+          let yMulti = 1 // event.deltaY / 80
+          let rotate = xMulti * yMulti
+          // event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
+          event.target.style.transform = 'translate(' + toX + 'px, ' + 100 + 'px) rotate(' + rotate + 'deg)'
 
-	Card.prototype.onPanEnd = function onPanEnd(event) {
-		if (this.props.isSwipeEnabled !== false) {
-			if (this.props.isLeftSwipeEnabled !== false && event.deltaX <= 0 || this.props.isRightSwipeEnabled !== false && event.deltaX >= 0) {
-				var newClass = this.state.classList.filter(function (s) {
-					return s !== 'moving';
-				});
-				this.setState({ classList: newClass });
-				var moveOutWidth = document.body.clientWidth;
-				var keep = Math.abs(event.deltaX) < 80;
-				event.target.classList.toggle('removed', !keep);
-				if (keep) {
-					event.target.style.transform = '';
-				} else {
-					var endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth);
-					var toX = event.deltaX > 0 ? endX : -endX;
-					var endY = Math.abs(event.velocityY) * moveOutWidth;
-					var toY = event.deltaY > 0 ? endY : -endY;
-					var xMulti = event.deltaX * 0.08; // 0.03
-					var yMulti = 1; // event.deltaY / 80
-					var rotate = xMulti * yMulti;
-					// event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
-					event.target.style.transform = 'translate(' + toX + 'px, ' + (100) + 'px) rotate(' + rotate + 'deg)';
+          // DO SWIPE ACTIONS
+          this.props.superOnSwipe()
+          if (this.props.onSwipe) this.props.onSwipe(this.props.data)
+          if (toX < 0 && this.props.onSwipeLeft) {
+            this.props.onSwipeLeft(this.props.data)
+          } else if (this.props.onSwipeRight) {
+            this.props.onSwipeRight(this.props.data)
+          }
+        }
+      }
+    }
+  }
 
-					// DO SWIPE ACTIONS
-					this.props.superOnSwipe();
-					if (this.props.onSwipe) this.props.onSwipe(this.props.data);
-					if (toX < 0 && this.props.onSwipeLeft) {
-						this.props.onSwipeLeft(this.props.data);
-					} else if (this.props.onSwipeRight) {
-						this.props.onSwipeRight(this.props.data);
-					}
-				}
-			}
-		}
-	};
+  Card.prototype.onDoubleTap = function onDoubleTap() {
+    if (this.props.onDoubleTap) this.props.onDoubleTap(this.props.data)
+  }
 
-	Card.prototype.onDoubleTap = function onDoubleTap() {
-		if (this.props.onDoubleTap) this.props.onDoubleTap(this.props.data);
-	};
+  Card.prototype.render = function render() {
+    return React.createElement(
+      Hammer,
+      { onPan: this.onPan.bind(this), onPanEnd: this.onPanEnd.bind(this), onDoubleTap: this.onDoubleTap.bind(this) },
+      React.createElement(
+        'div',
+        { className: this.state.classList.join(' '), style: this.props.style },
+        this.props.children
+      )
+    )
+  }
 
-	Card.prototype.render = function render() {
-		return React.createElement(
-			Hammer,
-			{ onPan: this.onPan.bind(this), onPanEnd: this.onPanEnd.bind(this), onDoubleTap: this.onDoubleTap.bind(this) },
-			React.createElement(
-				"div",
-				{ className: this.state.classList.join(" "), style: this.props.style },
-				this.props.children
-			)
-		);
-	};
+  return Card
+})(Component)
 
-	return Card;
-}(Component);
-
-export { Card as default };
+export { Card as default }
